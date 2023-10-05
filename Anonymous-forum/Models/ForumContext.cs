f531b4650a9c46b41d7c4423d39caaeb6fa3e1a9
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-10-05 12:55:34 +0200

    updated configuration of ForumContext

--- Anonymous-forum/Models/ForumContext.cs
+++ Anonymous-forum/Models/ForumContext.cs
@@ -17,5 +17,25 @@ namespace Anonymous_forum.Models
         public DbSet<Categories> Categories { get; set; } = null!;
         public DbSet<Comments> Comments { get; set; } = null!;
         public DbSet<Threads> Threads { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId); // Makes sure that this entity & all other entities have keys
+            });
+
+            modelBuilder.Entity<Comments>(entity =>
+            {
+                entity.HasKey(e => e.CommentId);
+            });
+
+            modelBuilder.Entity<Threads>(entity =>
+            {
+                entity.HasKey(e => e.ThreadId);
+            });
+
+            OnModelCreating(modelBuilder);
+        }
     }
 }
